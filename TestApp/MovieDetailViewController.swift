3c0a192ef36a4cca393ce63ae813395715f1a901
Roman Ivaniv <r.ivaniv1994@gmail.com>
2019-03-22 17:15:36 +0200

    Configure searchBar worklfow + base navigation

---
+++ TestApp/MovieDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MovieDetailViewController.swift
+//  TestApp
+//
+//  Created by Roman Ivaniv on 3/22/19.
+//  Copyright © 2019 Roman Ivaniv. All rights reserved.
+//
+
+import UIKit
+
+class MovieDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- TestApp/MoviesListTableViewController.swift
+++ TestApp/MoviesListTableViewController.swift
@@ -11,13 +11,22 @@ import UIKit
 private let movieCellIdentifier = "MovieCell"
 
 class MoviesListTableViewController: UITableViewController {
-
+    
+    @IBOutlet weak var headerSearchBar: UISearchBar!
+    
+    fileprivate var showSearchResult: Bool {
+        guard let text = headerSearchBar.text else {
+            return false
+        }
+        return !text.isEmpty
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-
+        tableView.setContentOffset(CGPoint(x: 0, y: headerSearchBar.frame.height), animated: false)
     }
-
+    
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
@@ -25,7 +34,7 @@ class MoviesListTableViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return 15
     }
     
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
@@ -35,8 +44,36 @@ class MoviesListTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath)
 
+        
         return cell
     }
-
    
+}
+
+extension MoviesListTableViewController: UISearchBarDelegate {
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        searchBar.setShowsCancelButton(true, animated: true)
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.text = ""
+        searchBar.deactivate()
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.deactivate()
+        tableView.reloadData()
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        //filter data
+    }
+}
+
+//create some helper file
+extension UISearchBar {
+    func deactivate(animated: Bool = true) {
+        resignFirstResponder()
+        setShowsCancelButton(false, animated: animated)
+    }
 }
