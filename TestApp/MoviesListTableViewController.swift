162e3116eb7deb702e799c370f0e9242987aa7e5
Roman Ivaniv <r.ivaniv1994@gmail.com>
2019-03-22 16:27:15 +0200

    Base setup + movie screen

---
+++ TestApp/MoviesListTableViewController.swift
@@ -0,0 +1,42 @@
+//
+//  MoviesListTableViewController.swift
+//  TestApp
+//
+//  Created by Roman Ivaniv on 3/22/19.
+//  Copyright © 2019 Roman Ivaniv. All rights reserved.
+//
+
+import UIKit
+
+private let movieCellIdentifier = "MovieCell"
+
+class MoviesListTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath)
+
+        return cell
+    }
+
+   
+}
